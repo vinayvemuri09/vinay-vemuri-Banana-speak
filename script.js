5514a0e288053ee09f2b800ebdfc94232f521371
vinayvemuri09 <vinayvemuri96@gmail.com>
2021-09-30 09:31:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#input");
+
+var translateAPI =
+  "https://api.funtranslations.com/translate/minion.json?text=";
+function errorHandler(error) {
+  console.log("The error is: ", error);
+  alert("There is some problem right now. Please try later!");
+}
+function clickHandler() {
+  fetch(translateAPI + inputText.value)
+    .then((response) => response.json())
+    .then((json) => {
+      output.innerText = json.contents.translated;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
